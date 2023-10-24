e0f922e50c3218618f11ca1f9a36951e88a6ecd1
Jared <126293208+indohippi@users.noreply.github.com>
2023-10-23 22:40:28 -0500

    styling updates

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Certifications() {
     return (
-        <section className="certifications-section">
+        <section className="certifications-section box-section">
             <h2>Certifications</h2>
             <ul>
                 <li>Web Developer - Fullstack Academy</li>
-                <li>Google Ads Search Certification - Google Digital Academy</li>
-                <li>AI-Powered Performance Ads Certification - Google Digital Academy</li>
-                <li>Google Analytics Individual Qualification</li>
+                <li>Google Ads Search Certification - Google</li>
+                <li>AI-Powered Performance Ads Certification - Google</li>
+                <li>Google Analytics Individual Qualification Certification - Google</li>
                 <li>Lean Six Sigma Certifications - Amazon</li>
                 <li>Learning Music Licensing - LinkedIn</li>
                 <li>Music Law: Copyrighting a Song – LinkedIn</li>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 function Skills() {
+    const skillsArray = [
+        "React.js", "JavaScript", "Full-Stack Development", "Back-End Web Development", 
+        "Software Development", "Front-End Development", "Node.js", "WordPress Development", 
+        "Responsive Web Design", "HTML", "SQL", "Data Analysis"
+    ];
+
     return (
-        <section className="skills-section">
+        <section className="skills-section box-section">
             <h2>Skills</h2>
             <div className="skills-list">
-                React.js, JavaScript, Full-Stack Development, Back-End Web Development, Software Development, Front-End Development, Node.js, WordPress Development, Responsive Web Design, HTML, SQL, Data Analysis
+                {skillsArray.map(skill => (
+                    <span key={skill}>{skill}</span>
+                ))}
             </div>
         </section>
     );
