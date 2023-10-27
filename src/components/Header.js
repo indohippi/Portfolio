13640087291281d4b66283b313ee1482349ce193
Jared <126293208+indohippi@users.noreply.github.com>
2023-10-26 20:39:51 -0500

    socials icons added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import profilePic1 from '../componentmedia/hwavelogo2.png';
+import profilePic1 from '../componentmedia/hwavelogo.png';
 import profilePic2 from '../componentmedia/D36DD8F1-C364-41E6-973D-FFF6BF99BF54.jpeg';
-import profilePic3 from '../componentmedia/hwavelogo2.png'; // Updated to relative path
+import profilePic3 from '../componentmedia/hwavelogo2.png';
+import linkedinIcon from '../componentmedia/linkedin.png';
+import emailIcon from '../componentmedia/e-mail-12.png';
+import githubIcon from '../componentmedia/githubicon.png';
 
 function Header() {
     return (
-        <header className="header-container">
-            <div className="images-container">
-                <div className="image-box">
-                    <img src={profilePic1} alt="Profile Picture 1" />
-                </div>
-                <div className="image-box">
-                    <img src={profilePic2} alt="Profile Picture 2" />
-                </div>
-                <div className="image-box">
-                    <img src={profilePic3} alt="Profile Picture 3" />
-                </div>
+        <header className="header">
+            <div className="header-images">
+                <img src={profilePic1} alt="Profile Pic 1" />
+                <img src={profilePic2} alt="Profile Pic 2" />
+                <img src={profilePic3} alt="Profile Pic 3" />
             </div>
-            <h1 className="glow-effect">Jared Wienen</h1>
-            <address className="contact-details">
-                Austin, United States 78626 <br/>
-                Tel: 4305022281 <br/>
-                Email: <a href="mailto:jaredwienen@gmail.com">jaredwienen@gmail.com</a> <br/>
-                LinkedIn: <a href="https://linkedin.com/in/jaredwienen" target="_blank" rel="noopener noreferrer">linkedin.com/in/jaredwienen</a>
-            </address>
-            <div className="links-container">
-                <a href="https://github.com/indohippi" target="_blank" rel="noopener noreferrer" className="profile-link">GitHub</a> | 
-                <a href="https://replit.com/@indohippi" target="_blank" rel="noopener noreferrer" className="profile-link">Repl.it</a> |
-                <a href="https://acc-capstone-jw.netlify.app/" target="_blank" rel="noopener noreferrer" className="profile-link">Full Stack Sample</a>
+            <div className="header-info">
+                <h1>Jared Wienen</h1>
+                <p>Austin, United States 78626</p>
+                <p>Tel: 4305022281</p>
+                <div className="header-links">
+                    <a href="mailto:jaredwienen@gmail.com">
+                        <img src={emailIcon} alt="Email Icon" />
+                    </a>
+                    <a href="https://linkedin.com/in/jaredwienen" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedinIcon} alt="LinkedIn Icon" />
+                    </a>
+                    <a href="https://github.com/indohippi" target="_blank" rel="noopener noreferrer">
+                        <img src={githubIcon} alt="GitHub Icon" />
+                    </a>
+                </div>
             </div>
         </header>
     );
